b95c1ff9d3a54adb9b8e83560c180065d84ffdfd
Robert Krency <bobkrency@gmail.com>
2023-01-06 01:11:33 -0500

    Added Readme Updates

--- droptimizer_aggregates.py
+++ droptimizer_aggregates.py
@@ -2,10 +2,13 @@ import requests
 import csv
 import os
 import pandas as pd
+from dotenv import load_dotenv
 from blizzardapi import BlizzardApi
 
 import gspread as gs
 import gspread_dataframe as gd
+
+load_dotenv()
 
 client_id = os.getenv('BLIZZ_CLIENT')
 client_secret = os.getenv('BLIZZ_SECRET')
@@ -135,34 +138,12 @@ tier_list = {
     '200428': ZENITH_SHOULDERS,
 }
 
-
-def import_reports():
-    '''
-    Downloads each report listed in the reports.txt file and writes to a csv file
-    '''
-    with open('data/reports.txt', 'r') as reports:
-        for report in reports:
-            report = report.strip()
-            print('Downloading report ' + report + '.')
-            # Download report csv
-            download = None
-            with requests.Session() as s:
-                download = s.get(report + '/data.csv')
-            decoded_content = download.content.decode('utf-8')
-
-            # Find the player name
-            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-            my_list = list(cr)
-            report_name = my_list[1][0]
-
-            # Write to File
-            print('Writing report ' + report + ' to file ' + report_name + '.csv')
-            with open('data/' + report_name + '.csv', 'w') as file:
-                file.write(decoded_content)
-
-
+# Item List from Blizz API for Caching
 item_list = {}
 def get_item(id):
+    '''
+    Retrieves item information from the Blizzard WoW GameData API using the item id.
+    '''
     if id in tier_list:
         id = tier_list[id]
     if id not in item_list:
@@ -171,10 +152,14 @@ def get_item(id):
     return item_list[id]
 
 
+# Boss List from Blizz API for Caching
 boss_list = {
     '-44': 'Trash',
 }
 def get_boss(id):
+    '''
+    Retrieves boss information from the Blizzard WoW GameData API using the encounter id.
+    '''
     if id not in boss_list:
         boss = api_client.wow.game_data.get_journal_encounter('us', 'en_US', id)
         boss_list[id] = boss['name']
@@ -194,20 +179,14 @@ def parse_report(data):
     baseline_dps = float(data[1][1])
     for sim in data[2:]:
         # Calculate the Sim Info
-        sim_name = sim[0]
-        name_list = sim_name.split('/')
         sim_dps = float(sim[1])
         sim_diff = (sim_dps - baseline_dps)
 
-        # Get the Boss Name
-        boss_id = name_list[1]
-        boss_name = get_boss(boss_id)
-        
-        # Get the Sim Item
-        item_id = name_list[3]
-        item_name = boss_name + ' - ' + get_item(item_id)
+        # Get the Item Name
+        sim_name_list = sim[0].split('/')
+        item_name = get_boss(sim_name_list[1]) + ' - ' + get_item(sim_name_list[3])
 
-        # Add to dictionary
+        # Add to data dictionary, choosing the highest sim if the item appears multiple times
         if item_name in report_data:
             report_data[item_name] = max(sim_diff, report_data[item_name])
         else:
@@ -216,26 +195,136 @@ def parse_report(data):
     return player, report_data
 
 
-def parse_reports():
+def get_report(report_link):
     '''
-    Iterates through the reports for parsing
+    Downloads the Report Data from Raidbots using the Simple CSV data endpoint.
     '''
-    print('Parsing reports...')
-    reports = [x for x in os.listdir(os.curdir + '/data') if x.endswith('.csv')]
-    all_data = {}
-    for report in reports:
-        with open('data/' + report) as csvfile:
-            cr = csv.reader(csvfile, delimiter=',')
-            player, data = parse_report(list(cr))
-            all_data[player] = data
-    dataframe = pd.DataFrame(data=all_data)
+    report_link = report_link + '/data.csv'
+    raw_data = None
+    with requests.Session() as s:
+        raw_data = s.get(report_link)
+    decoded_content = raw_data.content.decode('utf-8')
+    csv_data = csv.reader(decoded_content.splitlines(), delimiter=',')
+    data_list = list(csv_data)
+    return data_list
 
-    ws = gc.open(spreadsheet_name).worksheet("windbridge")
-    ws.clear()
-    gd.set_with_dataframe(worksheet=ws,dataframe=dataframe,include_index=True,include_column_header=True,resize=True)
+
+def write_dataframe_to_sheet(data, sheet):
+    '''
+    Writes a dataframe to the specified sheet
+    '''
+    sheet.clear()
+    gd.set_with_dataframe(worksheet=sheet,dataframe=data,include_index=True,include_column_header=True,resize=True)
+
+
+def get_boss_summary(data: dict):
+    '''
+    Grabs relevant statistics for each boss
+    '''
+    boss_data = {
+
+    }
+    for player in data:
+        player_data = {}
+        
+        # Get the highest upgrade for each Encounter
+        for item in data[player]:
+            item_list = item.split('-')
+            boss_name = item_list[0].strip()
+            item_name = item_list[1].strip()
+            if boss_name not in player_data:
+                player_data[boss_name] = 0
+            upgrade_value = max(0, data[player][item])
+            player_data[boss_name] = max(upgrade_value, player_data[boss_name])
+        
+        # Add player data to for boss
+        for boss in player_data:
+            # If the boss is not in the data, add it and initialize its value
+            if boss not in boss_data:
+                boss_data[boss] = {
+                    'count': 0,
+                    'total': 0,
+                    'max': 0
+                }
+            upgrade_value = player_data[boss]
+            if upgrade_value > 100:
+                boss_data[boss]['count'] += 1
+                boss_data[boss]['total'] += upgrade_value
+                boss_data[boss]['max'] = max(boss_data[boss]['max'], upgrade_value)
+
+    return boss_data
+
+
+def main():
+    # Open the main spreadsheet
+    spreadsheet = gc.open(os.getenv('SPREADSHEET_NAME'))
+
+    # Retrieve list of reports
+    links_sheet = spreadsheet.worksheet('Links')
+    mythic_reports_list = links_sheet.col_values(2)[1:]
+    heroic_reports_list = links_sheet.col_values(3)[1:]
+    normal_reports_list = links_sheet.col_values(4)[1:]
+
+    # Run Mythic Parses
+    mythic_dataframe = None
+    mythic_data = {}
+    if len(mythic_reports_list) > 0:
+        for report_link in mythic_reports_list:
+            # Download report data
+            report_data = get_report(report_link)
+            player, data = parse_report(report_data)
+            mythic_data[player] = data
+        mythic_dataframe = pd.DataFrame(data=mythic_data)
+        mythic_sheet = spreadsheet.worksheet('Mythic')
+        write_dataframe_to_sheet(mythic_dataframe, mythic_sheet)
+
+    # Run Heroic Parses
+    heroic_dataframe = None
+    heroic_data = {}
+    if len(heroic_reports_list) > 0:
+        for report_link in heroic_reports_list:
+            # Download report data
+            report_data = get_report(report_link)
+            player, data = parse_report(report_data)
+            heroic_data[player] = data
+        heroic_dataframe = pd.DataFrame(data=heroic_data)
+        heroic_sheet = spreadsheet.worksheet('Heroic')
+        write_dataframe_to_sheet(heroic_dataframe, heroic_sheet)
+
+    # Run Normal Parses
+    normal_dataframe = None
+    normal_data = {}
+    if len(normal_reports_list) > 0:
+        for report_link in normal_reports_list:
+            # Download report data
+            report_data = get_report(report_link)
+            player, data = parse_report(report_data)
+            normal_data[player] = data
+        normal_dataframe = pd.DataFrame(data=normal_data)
+        normal_sheet = spreadsheet.worksheet('Normal')
+        write_dataframe_to_sheet(normal_dataframe, normal_sheet)
+
+    # Get boss summaries
+    summary_sheet = spreadsheet.worksheet('Summary')
+    if mythic_dataframe is not None:
+        mythic_summary = get_boss_summary(mythic_data)
+        summary_dataframe = pd.DataFrame(data=mythic_summary).transpose().sort_index()
+        gd.set_with_dataframe(worksheet=summary_sheet, dataframe=summary_dataframe,
+            row=3, col=2, include_index=False, include_column_header=False)
+    if heroic_dataframe is not None:
+        heroic_summary = get_boss_summary(heroic_data)
+        summary_dataframe = pd.DataFrame(data=heroic_summary).transpose().sort_index()
+        gd.set_with_dataframe(worksheet=summary_sheet, dataframe=summary_dataframe,
+            row=3, col=5, include_index=False, include_column_header=False)
+    if normal_dataframe is not None:
+        normal_summary = get_boss_summary(normal_data)
+        summary_dataframe = pd.DataFrame(data=normal_summary).transpose().sort_index()
+        gd.set_with_dataframe(worksheet=summary_sheet, dataframe=summary_dataframe,
+            row=3, col=8, include_index=False, include_column_header=False)
+
+
 
 
 # Let's do this thing.
 if __name__ == '__main__':
-    import_reports()
-    parse_reports()
+    main()
